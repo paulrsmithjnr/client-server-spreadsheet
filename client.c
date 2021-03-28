a66dbb76dcb9eac8216e7b0f893f21dc1717b7e3
Paul Smith <paulrsmithjnr@gmail.com>
2021-03-28 18:05:22 -0500

    Server and Client can now: - communicate cell details back and forth - update the contents of their grids
    Client:
    - displays the updated spreadsheet each time a valid request is sent to the server
    - spreadsheet truncates values that are larger than the width of a cell

--- client.c
+++ client.c
@@ -10,16 +10,24 @@
 
 #define BUF_SIZE	1024
 #define	SERVER_IP	"127.0.0.1"
-#define SERVER_PORT	60000
+#define SERVER_PORT	2122
+#define NUM_RANGE 9
 
-void createSpreadsheet();
+//function declarations
+void drawSpreadsheet();
+void getNewSpreadsheet();
+int colLetterToNum(char letter);
+void placeOnGrid(int x, int y, char* c);
+
+//global declaration structure grid
+char * grid[NUM_RANGE][NUM_RANGE];
 
 int main() {
     int	sock_send;
     struct sockaddr_in	addr_send;
     int	i;
-    char text[80],buf[BUF_SIZE];
-    int	send_len,bytes_sent;
+    char cellAddr[4], *addr, *val, cellVal[80], details[90], buf[BUF_SIZE];
+    int	send_len,bytes_sent, bytes_received;
 
         /* create socket for sending data */
     sock_send=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -40,13 +48,15 @@ int main() {
         printf("connect() failed\n");
         exit(0);
     }
+    getNewSpreadsheet();
+    drawSpreadsheet();
+
     int flag=1;
     while (flag){
         /* send some data */
-        printf("Enter the cell address you would like to edit (Please enter quit to leave): ");
-        scanf("%s",text);
-        if (strcmp(text,"quit") == 0){
-            // strcpy(buf,text);
+        printf("\nEnter the cell address you would like to edit (Please enter quit to leave): ");
+        scanf("%s",cellAddr);
+        if (strcmp(cellAddr,"quit") == 0){
             printf("Bye!!!\n");
             strcpy(buf,"shutdown");
             send_len=strlen("shutdown");
@@ -54,24 +64,126 @@ int main() {
             flag=0;
             break;
         }
-        createSpreadsheet();
-        
+
+        printf("Enter value to input into the selected cell: ");
+        scanf("%s", cellVal);
+
+        //send cell details (address:value) to server
+        strcpy(details, cellAddr);
+        strcat(details, ":");
+        strcat(details, cellVal);
+        strcpy(buf, details);
+        send_len=strlen(details);
+        bytes_sent=send(sock_send,buf,send_len,0);
+
+        //receive broadcasted cell details from server
+        bytes_received=recv(sock_send,buf,BUF_SIZE,0);
+        buf[bytes_received]=0;
+        addr = strtok(buf, ":");
+        val = strtok(NULL, ":");
+
+        int y = colLetterToNum(addr[0]);
+        int x = addr[1] - '0';
+        placeOnGrid(x, y, val);
+        drawSpreadsheet();
     }
 
-    createSpreadsheet();
     close(sock_send);
     return 0;
 }
 
-void createSpreadsheet() {
+void getNewSpreadsheet() {
+    for(int i = 0; i < NUM_RANGE; i++) {
+        for(int j = 0; j < NUM_RANGE; j++) {
+            grid[i][j] = malloc(sizeof(char *));
+            strcpy(grid[i][j], " ");
+        }
+    }
+}
+
+void drawSpreadsheet() {
     char * const COLUMN_HEADINGS = "      A        B        C        D        E        F        G        H        I";
     char * const HORIZONTAL_LINE = "  +--------+--------+--------+--------+--------+--------+--------+--------+--------+";
-    char * const VERTICAL__LINES = "|        |        |        |        |        |        |        |        |        |";
-    
-    printf("%s\n", COLUMN_HEADINGS);
-    for(int i = 0; i < 9; i++) {
+    int const cellWidthPaddingVal = 8;
+
+    printf("\n\n%s\n", COLUMN_HEADINGS);
+    for(int i = 0; i < NUM_RANGE; i++) {
         printf("%s\n", HORIZONTAL_LINE);
-        printf("%d %s\n",i+1, VERTICAL__LINES);
+        printf("%d ", i+1);
+        for(int j = 0; j < NUM_RANGE; j++) {
+            if(strcmp(grid[i][j], " ") == 0) {
+                printf("|        ");
+            } else {
+                printf("|");
+                int len = strlen(grid[i][j]);
+                if(len > cellWidthPaddingVal) {
+
+                    //cell content truncation
+                    for(int l = 0; l < cellWidthPaddingVal - 3; l++) {
+                        printf("%c", grid[i][j][l]);
+                    }
+                    printf("...");
+
+                } else {
+                    printf("%s", grid[i][j]);
+                    for(int k = 0; k < cellWidthPaddingVal - strlen(grid[i][j]); k++) {
+                    printf(" ");
+                    }
+                }
+            }
+        }
+        printf("|\n");
     }
     printf("%s\n", HORIZONTAL_LINE);
+}
+
+//TODO: Remove this function and have server broadcast the actual coordinates on the grid to place the value
+int colLetterToNum(char letter) {
+    switch(letter) {
+        case 'A':
+        case 'a':
+            return 1;
+        
+        case 'B':
+        case 'b':
+            return 2;
+        
+        case 'C':
+        case 'c':
+            return 3;
+
+        case 'D':
+        case 'd':
+            return 4;
+
+        case 'E':
+        case 'e':
+            return 5;
+
+        case 'F':
+        case 'f':
+            return 6;
+
+        case 'G':
+        case 'g':
+            return 7;
+
+        case 'H':
+        case 'h':
+            return 8;
+
+        case 'I':
+        case 'i':
+            return 9;
+        
+        default:
+            return 0;
+    }
+}
+
+void placeOnGrid(int x, int y, char* c){
+    char *string = malloc(sizeof(char *));
+    strcpy(string, c);
+    grid[x-1][y-1]=string;
+    return;
 }
--- server.c
+++ server.c
@@ -7,7 +7,18 @@
 #include <unistd.h>
 
 #define BUF_SIZE	1024
-#define LISTEN_PORT	60000
+#define LISTEN_PORT	2122
+#define NUM_RANGE 9
+
+//function declarations
+void getNewSpreadsheet();
+void placeOnGrid(int x, int y, char* c);
+int checkGridSpot(int x, int y);
+int validatePosition(int x, int y);
+int colLetterToNum(char letter);
+
+//global declaration structure grid
+char * grid[NUM_RANGE][NUM_RANGE];
 
 int main() {
     int	sock_listen,sock_recv;
@@ -16,11 +27,13 @@ int main() {
     int	incoming_len;
     struct sockaddr	remote_addr;
     int	recv_msg_size;
+    int send_len,bytes_sent;
     char buf[BUF_SIZE];
-    int const NUM_RANGE=9;
+    char *cellAddr, *cellVal, details[90];
+    // int const NUM_RANGE=9;
 
     //global declaration structure grid
-    char * grid[NUM_RANGE][NUM_RANGE];
+    // char * grid[NUM_RANGE][NUM_RANGE];
 
 
             /* create socket for listening */
@@ -50,7 +63,8 @@ int main() {
         printf("listen() failed\n");
         exit(0);
     }
-     
+    
+    
  
         /* get new socket to receive data on */
         /* It extracts the first connection request from the  */
@@ -58,47 +72,119 @@ int main() {
     addr_size=sizeof(recv_addr);
     sock_recv=accept(sock_listen, (struct sockaddr *) &recv_addr, &addr_size);
     int flag=1;
+    getNewSpreadsheet();
     while (flag){
+
+        //receive cell details (address:value)
         bytes_received=recv(sock_recv,buf,BUF_SIZE,0);
         buf[bytes_received]=0;
-        printf("Received: %s\n",buf);
         if (strcmp(buf,"shutdown") == 0){
+            printf("Received: %s ",buf);
             flag=0;
             break;
         }
+        cellAddr = strtok(buf, ":");
+        cellVal = strtok(NULL, ":");
+        printf("Received: %s -> %s\n", cellVal, cellAddr);
+
+        if (strlen(cellAddr) == 2) {
+            int y = colLetterToNum(cellAddr[0]);
+            int x = cellAddr[1] - '0';
+            placeOnGrid(x, y, cellVal);
+
+            //broadcast cell details (address:value) to all clients
+            strcpy(details, cellAddr);
+            strcat(details, ":");
+            strcat(details, cellVal);
+            strcpy(buf, details);
+            send_len=strlen(details);
+            bytes_sent=send(sock_recv,buf,send_len,0);
+        }
+        
     }
-    
+    printf("\n");
     close(sock_recv);
     close(sock_listen);
     return 0;
 }
 
+void getNewSpreadsheet() {
+    for(int i = 0; i < NUM_RANGE; i++) {
+        for(int j = 0; j < NUM_RANGE; j++) {
+            grid[i][j] = malloc(sizeof(char *));
+            strcpy(grid[i][j], " ");
+        }
+    }
+}
+
 //place value on position
 void placeOnGrid(int x, int y, char* c){
-    grid[x-1][y-1]=c;
+    char *string = malloc(sizeof(char *));
+    strcpy(string, c);
+    grid[x-1][y-1]=string;
     return;
 }
 
 //checks if spot on grid is already filled
-bool checkGridSpot(int x, int y){
+int checkGridSpot(int x, int y){
     printf("%s \n",grid[x-1][y-1] );
     if(*grid[x-1][y-1] == ' '){
-        return true;
+        return 1;
     }else{
-        return false;
+        return 0;
     }
 
 }
 
 //checks if coordinates is on the board
-bool validatePosition(int x, int y){
+int validatePosition(int x, int y){
     if(x == 0 || y == 0){
-        return false;
+        return 0;
     }
     if(x <= NUM_RANGE && y <= NUM_RANGE ){
-        return true;
-    }else{return false;}
-
+        return 1;
+    }else{return 0;}
 }
 
-//TODO create function to convert grid to string to send to client
+int colLetterToNum(char letter) {
+    switch(letter) {
+        case 'A':
+        case 'a':
+            return 1;
+        
+        case 'B':
+        case 'b':
+            return 2;
+        
+        case 'C':
+        case 'c':
+            return 3;
+
+        case 'D':
+        case 'd':
+            return 4;
+
+        case 'E':
+        case 'e':
+            return 5;
+
+        case 'F':
+        case 'f':
+            return 6;
+
+        case 'G':
+        case 'g':
+            return 7;
+
+        case 'H':
+        case 'h':
+            return 8;
+
+        case 'I':
+        case 'i':
+            return 9;
+        
+        default:
+            return 0;
+    }
+}
