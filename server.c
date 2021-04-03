a3004b8df3da851458bd902f908c843daf5bc68b
Paul Smith <paulrsmithjnr@gmail.com>
2021-04-03 01:28:23 -0500

    Added average function

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>	/* exit() warnings */
 #include <string.h>	/* memset warnings */
 #include <unistd.h>
+#include <ctype.h>
 
 #define BUF_SIZE	1024
 #define LISTEN_PORT	2121
@@ -16,6 +17,10 @@ void placeOnGrid(int x, int y, char* c);
 int checkGridSpot(int x, int y);
 int validatePosition(int x, int y);
 int colLetterToNum(char letter);
+int isNumber(char *string);
+int power(int base, int exp);
+int stringToNumber(char *string);
+double average(char *start, char *end);
 
 //global declaration structure grid
 char * grid[NUM_RANGE][NUM_RANGE];
@@ -83,9 +88,35 @@ int main() {
             printf("Received: %s ",buf);
             break;
         }
+        
         cellAddr = strtok(buf, ":");
         cellVal = strtok(NULL, ":");
         printf("Received: %s -> %s\n", cellVal, cellAddr);
+
+        if((cellVal[0] == '=') && ((cellVal[1] == 'A') || (cellVal[1] == 'a'))) { //checking for the average function
+
+            char *avgParam1 = strtok(cellVal, "(");
+            avgParam1 = strtok(NULL, "(");
+            avgParam1 = strtok(avgParam1, ","); //first parameter stored here
+
+            char *avgParam2 = strtok(NULL, ",");
+            avgParam2[strlen(avgParam2)-1] = '\0'; //second parameter stored here
+
+            printf("\nParams: %s, %s\n", avgParam1, avgParam2);
+
+            if((strlen(avgParam1) != 2) || (strlen(avgParam2) != 2)) {
+                strcpy(cellAddr, "00"); //will evoke a pre-handled error - message on the client side
+            } else {
+                double test = average(avgParam1, avgParam2); //TODO: assign return val to cellVal
+                printf("\nTest Average: %.2lf\n", test);
+            }
+
+
+            strcpy(cellVal, "average");
+
+        } else if((cellVal[0] == '=') && ((cellVal[1] == 'S') || (cellVal[1] == 's'))) { //checking for the sum function
+            strcpy(cellVal, "sum");
+        }
 
         if (strlen(cellAddr) == 2) {
             y = colLetterToNum(cellAddr[0]);
@@ -188,4 +219,57 @@ int colLetterToNum(char letter) {
         default:
             return 0;
     }
+}
+
+int isNumber(char *string) {
+    int flag = 1;
+    for(int i = 0; i < strlen(string); i++) {
+        if(isdigit(string[i]) == 0) {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+int power(int base, int exp) {
+    int result = 1;
+    while(exp != 0) {
+        result *= base;
+        exp--;
+    }
+    return result;
+}
+
+int stringToNumber(char *string) {
+    int number = 0;
+    for(int i = 0; i < strlen(string); i++) {
+        number += (string[i] - '0') * power(10, (strlen(string) - (i+1)));
+    }
+    return number;
+}
+
+double average(char *start, char *end) {
+    double avg = 0, count = 0;
+    int x1 = start[1] - '0', x2 = end[1] - '0';
+    int y1 = colLetterToNum(start[0]), y2 = colLetterToNum(end[0]);
+    printf("\n(x1,y1) (x2, y2): (%d, %d) (%d, %d)\n", x1, y1, x2, y2);
+    for(int x = x1 - 1; x < ((x1 - 1) + ((x2 - x1)+1)); x++) {
+        for(int y = y1 - 1; y <  ((y1 - 1) + ((y2 - y1)+1)); y++) {
+            printf("\nGrid (%d, %d): %s\n", x, y, grid[x][y]);
+            if(isNumber(grid[x][y])) {
+                printf("\nString to Number: %d\n", stringToNumber(grid[x][y]));
+                avg += stringToNumber(grid[x][y]);
+                count++;
+            } else if(strcmp(grid[x][y], " ") == 0) {
+                count++;
+            } else {
+                return -1.0;
+            }
+        }
+    }
+    if(count == 0) {
+        return 0.0;
+    }
+    return avg/count;
 }
