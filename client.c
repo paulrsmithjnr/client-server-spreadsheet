90004a196f0a72de326ea6e94a7512148db6c884
Paul Smith <paulrsmithjnr@gmail.com>
2021-04-05 00:10:23 -0500

    Started setting up client code to facilitate multithreading - work somewhat

--- client.c
+++ client.c
@@ -7,14 +7,18 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <pthread.h>
 
 
-#define BUF_SIZE	1024
+#define BUFFER_SIZE	1024
 #define	SERVER_IP	"127.0.0.1"
 #define SERVER_PORT	2121
 #define NUM_RANGE 9
 
 //function declarations
+void receiveFromServer();
+void sendToServer();
+void printPrompt();
 void drawSpreadsheet();
 void getNewSpreadsheet();
 int colLetterToNum(char letter);
@@ -23,12 +27,15 @@ void placeOnGrid(int x, int y, char* c);
 //global declaration structure grid
 char * grid[NUM_RANGE][NUM_RANGE];
 
+//global socket declaration
+int sock_send;
+
+//global declaration of flag
+static volatile int endFlag = 0;
+
 int main() {
-    int	sock_send;
     struct sockaddr_in	addr_send;
     int	i;
-    char cellAddr[4], *addr, *val, cellVal[80], details[90], buf[BUF_SIZE];
-    int	send_len,bytes_sent, bytes_received;
 
         /* create socket for sending data */
     sock_send=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -52,17 +59,114 @@ int main() {
     getNewSpreadsheet();
     drawSpreadsheet();
 
-    int flag=1;
-    while (flag){
+    pthread_t sendThread;
+    int sendVal = pthread_create(&sendThread, NULL, (void *) sendToServer, NULL);
+    if(sendVal != 0){
+		printf("\n[-] Error setting up sending thread\n");
+        return -1;
+	}
+
+	pthread_t receiveThread;
+    int receiveVal = pthread_create(&receiveThread, NULL, (void *) receiveFromServer, NULL);
+    if(receiveVal != 0){
+        printf("\n[-] Error setting up receiving thread\n");
+        return -1;
+    }
+
+    while (1){
+        // /* send some data */
+        // printf("\nEnter the cell address you would like to edit (Please enter quit to leave): ");
+        // scanf("%s",cellAddr);
+        // if (strcmp(cellAddr,"quit") == 0){
+        //     printf("Bye!!!\n");
+        //     strcpy(buf,"shutdown");
+        //     send_len=strlen("shutdown");
+        //     bytes_sent=send(sock_send,buf,send_len,0);
+        //     break;
+        // } else if(strlen(cellAddr) != 2) {
+        //     printf("\n ** ERROR: Invalid cell address **\n");
+        //     continue;
+        // } else if((isalpha(cellAddr[0]) == 0) || isdigit(cellAddr[1]) == 0) {
+        //     printf("\n ** ERROR: Invalid cell address **\n");
+        //     continue;
+        // }
+
+        // printf("Enter value to input into the selected cell: ");
+        // scanf("%s", cellVal);
+
+        // //send cell details (address:value) to server
+        // strcpy(details, cellAddr);
+        // strcat(details, ":");
+        // strcat(details, cellVal);
+        // strcpy(buf, details);
+        // send_len=strlen(details);
+        // bytes_sent=send(sock_send,buf,send_len,0);
+
+        // //receive broadcasted cell details from server
+        // bytes_received=recv(sock_send,buf,BUF_SIZE,0);
+        // buf[bytes_received]=0;
+        // addr = strtok(buf, ":");
+        // val = strtok(NULL, ":");
+
+        // int x = addr[0] - '0';
+        // int y = addr[1] - '0';
+        // if((x == 0) || (y == 0)) {
+        //     printf("\n ** ERROR: Invalid request (ignored) **\n");
+        //     continue;
+        // }
+        // placeOnGrid(x, y, val);
+        // drawSpreadsheet();
+        if(endFlag) {
+            break;
+        }
+    }
+
+    close(sock_send);
+    return 0;
+}
+
+void receiveFromServer() {
+    char buffer[BUFFER_SIZE], *addr, *val;
+    int bytes_received;
+
+    //receive broadcasted cell details from server
+    while(1) {
+        bytes_received=recv(sock_send, buffer, BUFFER_SIZE,0);
+
+        buffer[bytes_received]=0;
+        addr = strtok(buffer, ":");
+        val = strtok(NULL, ":");
+
+        int x = addr[0] - '0';
+        int y = addr[1] - '0';
+        if((x == 0) || (y == 0)) {
+            printf("\n ** ERROR: Invalid request (ignored) **\n");
+            continue;
+        }
+        placeOnGrid(x, y, val);
+        drawSpreadsheet();
+        printPrompt();
+
+        if(endFlag) {
+            break;
+        }
+    }
+}
+
+void sendToServer() {
+    int	send_len, bytes_sent;
+    char buffer[BUFFER_SIZE], cellAddr[4], cellVal[80], details[90];
+
+    while(1) {
         /* send some data */
-        printf("\nEnter the cell address you would like to edit (Please enter quit to leave): ");
+        printPrompt();
         scanf("%s",cellAddr);
         if (strcmp(cellAddr,"quit") == 0){
             printf("Bye!!!\n");
-            strcpy(buf,"shutdown");
-            send_len=strlen("shutdown");
-            bytes_sent=send(sock_send,buf,send_len,0);
-            flag=0;
+            strcpy(buffer, "shutdown");
+            send_len = strlen("shutdown");
+            bytes_sent = send(sock_send, buffer, send_len, 0);
+            endFlag = 1;
             break;
         } else if(strlen(cellAddr) != 2) {
             printf("\n ** ERROR: Invalid cell address **\n");
@@ -79,28 +183,16 @@ int main() {
         strcpy(details, cellAddr);
         strcat(details, ":");
         strcat(details, cellVal);
-        strcpy(buf, details);
+        strcpy(buffer, details);
         send_len=strlen(details);
-        bytes_sent=send(sock_send,buf,send_len,0);
-
-        //receive broadcasted cell details from server
-        bytes_received=recv(sock_send,buf,BUF_SIZE,0);
-        buf[bytes_received]=0;
-        addr = strtok(buf, ":");
-        val = strtok(NULL, ":");
-
-        int x = addr[0] - '0';
-        int y = addr[1] - '0';
-        if((x == 0) || (y == 0)) {
-            printf("\n ** ERROR: Invalid request (ignored) **\n");
-            continue;
-        }
-        placeOnGrid(x, y, val);
-        drawSpreadsheet();
+        bytes_sent=send(sock_send,buffer,send_len,0);
     }
+    return;
+}
 
-    close(sock_send);
-    return 0;
+void printPrompt() {
+    printf("\nEnter the cell address you would like to edit (Please enter quit to leave): ");
+    fflush(stdout);
 }
 
 void getNewSpreadsheet() {
